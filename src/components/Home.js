2a6a01cb546bb44d2b01dd1f53234eb3fb270a69
NalloluSatishReddy9720 <satishreddynallolla@gmail.com>
2024-03-11 12:10:03 +0530

    changes done in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function Home() {
   return ( 
-    <>This is home</>
+    <>
+    <h1>this is home</h1>
+    <p>you can check the nav bar and click on about and textutils for more usage of this page</p>
+    </>
   );
 }
